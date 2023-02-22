e7469656d0070aca3e84e2afa0a27289a503b2f1
Svyatoslav <svyatyar2@gmail.com>
2023-02-22 13:32:46 +0300

    [ADD] routing for frontend application

--- number-frontend/src/app/app-routing.module.ts
+++ number-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NumberListComponent } from './number-base/number-list/number-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'numbers', pathMatch: 'full'},
+  { path: 'numbers', component: NumberListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
