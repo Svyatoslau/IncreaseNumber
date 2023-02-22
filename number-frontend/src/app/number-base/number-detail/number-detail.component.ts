ef2eb095376aa5dfb77e258254ed108bed5abff6
Svyatoslav <svyatyar2@gmail.com>
2023-02-22 13:31:40 +0300

    [ADD] number-detail component

--- number-frontend/src/app/number-base/number-detail/number-detail.component.ts
+++ number-frontend/src/app/number-base/number-detail/number-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NumberService } from 'src/app/services/number.service';
+import { INumber } from '../models/inumber';
+import { Increment } from '../models/increment';
 
 @Component({
   selector: 'app-number-detail',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./number-detail.component.css']
 })
 export class NumberDetailComponent implements OnInit {
+  @Input()
+  public number!: INumber;
+  @Input()
+  public incr!: number;
 
-  constructor() { }
+  constructor(private numberService: NumberService) { }
 
   ngOnInit() {
+
+  }
+
+  increaseNumber(incr: number, id: number): void{
+    let increment: Increment = {
+      value: incr
+    }
+
+    this.numberService.increaseNumber(id, increment)
+      .subscribe(
+        (num: INumber) => this.number = num
+      );
   }
 
 }
