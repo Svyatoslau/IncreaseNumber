e9606507e2887d3e46d59212431459bb5ebaccb2
Svyatoslav <svyatyar2@gmail.com>
2023-02-23 15:42:26 +0300

    [FIX] name of entities and cod style in db context

---
+++ IncreaseNumberAPI/DAL/Entities/Counter.cs
@@ -0,0 +1,9 @@
+﻿namespace IncreaseNumberAPI.DAL.Entities;
+
+public class Counter
+{
+    public int Id { get; set; }
+    public int Value { get; set; }
+    public DateTime LastChange { get; set; }
+    public DateTime PickedDate { get; set; }
+}
