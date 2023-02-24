72de0fdb33cf3ca9d9851240dcf34cd0714a8ad2
Svyatoslav <svyatyar2@gmail.com>
2023-02-24 11:08:38 +0300

    [ADD] material button

--- number-frontend/src/app/counter-base/counter-base.module.ts
+++ number-frontend/src/app/counter-base/counter-base.module.ts
@@ -5,22 +5,40 @@ import { CounterListComponent } from './counter-list/counter-list.component';
 import { CounterDetailComponent } from './counter-detail/counter-detail.component';
 import { CounterService } from '../services/counter.service';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { DatetimePickerComponent } from './datetime-picker/datetime-picker.component';
+import { NgxMatDatetimePickerModule, NgxMatNativeDateModule } from '@angular-material-components/datetime-picker';
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    MatDatepickerModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    NgxMatDatetimePickerModule,
+    NgxMatNativeDateModule
   ],
   declarations: [
     CounterListComponent,
-    CounterDetailComponent
+    CounterDetailComponent,
+    DatetimePickerComponent
   ],
   exports: [
     CounterListComponent,
-    CounterDetailComponent
+    CounterDetailComponent,
+    DatetimePickerComponent, 
+    
   ],
   providers: [
-    CounterService
+    CounterService,
+    MatDatepickerModule
   ]
 })
 export class CounterBaseModule { }
